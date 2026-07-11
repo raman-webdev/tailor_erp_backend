ff51f861b613e66a49d3829a7983bd5078f0679e
raman-webdev <chaudharyraman562@gmail.com>
2026-07-12 01:00:30 +0545

    register branch to django admin

--- apps/organizations/admin.py
+++ apps/organizations/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Organization
+from .models import Organization, Branch
 
 # Register your models here.
 
 admin.site.register(Organization)
+admin.site.register(Branch)
